4b010d94e948025267f6227902a513898fd2941a
juniata <kenta.github@gmail.com>
2017-02-19 19:31:01 +0900

    武器クラス
    親クラスと子クラスの作成

---
+++ ActionGame/Assets/Scene/Miki/Script/WeaponGun.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponGun : Weapon {
+
+    // 使用している武器のタイプを返す。
+    public override USE_WEAPON_TYPE GetWeaponType()
+    {
+        return USE_WEAPON_TYPE.GUN;
+    }
+}
---
+++ ActionGame/Assets/Scene/Miki/Script/WeaponSickle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSickle : Weapon {
+
+    // 使用している武器のタイプを返す。
+    public override USE_WEAPON_TYPE GetWeaponType()
+    {
+        return USE_WEAPON_TYPE.SICKLE;
+    }
+}

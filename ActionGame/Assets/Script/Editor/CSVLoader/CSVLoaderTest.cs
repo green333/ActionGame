30a717ff2fd0c24f10a7ce0fff136ccead85c5ad
juniata <kenta.github@gmail.com>
2017-04-09 10:54:31 +0900

    SCVLoaderの単体テストを作成
    LoadCSVAll()のテストのみ追加

---
+++ ActionGame/Assets/Script/Editor/CSVLoader/CSVLoaderTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+[TestFixture]
+public class CSVLoaderTest
+{
+    private CSVLoader csvLoader = null;
+
+    [SetUp]
+    public void TestInit()
+    {
+        // CSVのインスタンスを取得
+        csvLoader = CSVLoader.Instance;
+    }
+
+    public static TestCaseData[] loadCSVAllProvider = new[]
+    {
+        // パスが正しく、日本語、英語、数字が正常に読み込まれているかのテスト
+        new TestCaseData(Application.dataPath + "/Script/Editor/CSVLoader/fixture_loadVCSVAll.csv", new List<string> {"1","2","3","4","5","aiueo","あいうえお","123test" }).SetName("test loadCSVAll() SUCCEED"),
+        
+        // パスが間違っているときのテスト
+        new TestCaseData(Application.dataPath + "/CSVLoader/fixture_loadVCSVAll.csv",null).SetName("test loadCSVAll() FAILED")
+    };
+
+    [TestCaseSource("loadCSVAllProvider")]
+    public void TestLoadCSVAll(string filepath, List<string> funcRetResult)
+    {
+        List<string> result = csvLoader.LoadCSVAll(filepath);
+
+        if (result == null)
+        {
+            Assert.AreEqual(result, funcRetResult);
+        }else
+        {
+            Assert.AreEqual(result.ToArray(), funcRetResult.ToArray());
+        }
+    }
+
+}

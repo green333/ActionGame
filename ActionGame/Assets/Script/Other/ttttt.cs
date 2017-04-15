fd9887cedaf5d111ca5ae80c4ee21a0172dc6cfa
KTYN <2130001@ecccomp.com>
2017-04-15 11:18:24 +0900

    PlayerPrefsラッパー追加

---
+++ ActionGame/Assets/Script/Other/ttttt.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class Unko
+{
+    public string name;
+    public int coin;
+
+    public Unko(string name, int coin)
+    {
+        this.name = name;
+        this.coin = coin;
+    }
+}
+
+public class ttttt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefsWrap.Instance.DeleteAll();
+        //List<int> tinko = new List<int>();
+        //tinko.Add(11);
+        //tinko.Add(22);
+        //tinko.Add(33);
+        //tinko.Add(44);
+        //tinko.Add(55);
+        //Debug.Log("tinko:" + tinko.Count);
+        //PlayerPrefsWrap.Instance.DeleteAll();
+        //PlayerPrefsWrap.Instance.SaveGeneric<List<int>>("list", tinko);
+        //List<int> intList = new List<int>(PlayerPrefsWrap.Instance.LoadGeneric<List<int>>("list"));
+        //Debug.Log(intList.Count);
+        //for (int i = 0; i < intList.Count; i++)
+        //{
+        //    Debug.Log(i.ToString() + "：" + intList[i]);
+        //}
+        Unko unko = new Unko("名前", 1000);
+        PlayerPrefsWrap.Instance.SaveGeneric<Unko>("me", unko);
+        Unko tinko = PlayerPrefsWrap.Instance.LoadGeneric<Unko>("me");
+        Debug.Log(tinko.name);
+        Debug.Log(tinko.coin);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

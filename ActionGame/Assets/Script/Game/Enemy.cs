14219fdc9c932117be2c7fafcdac4f7592983aae
KTYN <2130001@ecccomp.com>
2017-05-05 00:27:30 +0900

    いろいろ作った
    エネミーとプレイヤーとセーブデータクラス

---
+++ ActionGame/Assets/Script/Game/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    EnemyBaseMaster.Param param;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ActionGame/Assets/Script/Game/SaveData.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// セーブデータクラス
+/// </summary>
+public class SaveData
+{
+    /// <summary>
+    /// シングルトン
+    /// </summary>
+    private static SaveData instance = new SaveData();
+    public static SaveData Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    /// <summary>
+    /// セーブ用キー定数
+    /// </summary>
+    private const string KEY_CHAPTER = "chapter";
+    private const string KEY_STAGE_ID = "stageId";
+
+    /// <summary> 章 </summary>
+    public int chapter { get; set; }
+    /// <summary> ステージID </summary>
+    public int stageId { get; set; }
+
+    /// <summary>
+    /// セーブ
+    /// </summary>
+    public void Save()
+    {
+        PlayerPrefsWrap.Instance.SaveInt(KEY_CHAPTER, chapter);
+        PlayerPrefsWrap.Instance.SaveInt(KEY_STAGE_ID, stageId);
+    }
+
+    //  ------------ ロードとりあえず個々に ------------
+    /// <summary>
+    /// 章ロード
+    /// </summary>
+    /// <returns></returns>
+    public int LoadChapter()
+    {
+        return PlayerPrefsWrap.Instance.LoadInt(KEY_CHAPTER);
+    }
+
+    /// <summary>
+    /// ステージIDロード
+    /// </summary>
+    /// <returns></returns>
+    public int LoadStageID()
+    {
+        return PlayerPrefsWrap.Instance.LoadInt(KEY_STAGE_ID);
+    }
+}

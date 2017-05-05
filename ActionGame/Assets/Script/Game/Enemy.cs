c54929a36fb7e38ed0c0f095dd1554698b59780b
KTYN <2130001@ecccomp.com>
2017-05-05 11:43:28 +0900

    EnemyとSaveData変更

--- ActionGame/Assets/Script/Game/Enemy.cs
+++ ActionGame/Assets/Script/Game/Enemy.cs
@@ -1,12 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour {
 
-    EnemyBaseMaster.Param param;
+    List<EnemyBaseMaster.Param> baseParamList;
+    List<EnemyGrowthMaster.Param> growthParamList;
 
-	// Use this for initialization
+    /*
+     * EnemyBaseMaster.Paramを変数として持たせること。
+     * 
+     * セーブデータクラスが保持している章とステージIDを使用してLoadEnemySpawnMasterのGetEnemySpawanInfo（）から出現する敵の情報を取得。
+     * 
+     * 取得した敵出現情報をLoadEnemyBaseMasterのGetEnemyInfo()に投げて、出現する敵の基本情報を取得する。
+     * 
+     * LoadEnemySpawnMaster()とGetEnemyInfo()から取得したデータと、
+     * プレイヤーのレベルを使用してLoadEnemyGrowthMasterクラスのGetEnemyInfo()から敵のステータス情報を取得する。
+     * 
+     * なお出現する敵の数はGetEnemySpawanInfo()で取得した情報の中に入っているので、その数だけ敵を生成すること。
+     * 
+     * (現在プレイヤークラスにレベル変数を持たせていないので、敵のステータスを取得するときは引数に１を与えること。)
+     */
+
+    // Use this for initialization
 	void Start () {
+        SaveData.Instance.stageId = 1;
+        SaveData.Instance.chapter = 1;
+        EnemySpawnMaster.Param enemySpawnParam = LoadEnemySpawnMaster.GetInstance.GetEnemySpawanInfo(SaveData.Instance.stageId, SaveData.Instance.chapter);
+        LoadEnemyBaseMaster.GetInstance.GetEnemyInfo(out baseParamList, enemySpawnParam);
+        LoadEnemyGrowthMaster.GetInstance.GetEnemyInfo(out growthParamList, baseParamList, enemySpawnParam, 1);
+
+        foreach(EnemyGrowthMaster.Param p in this.growthParamList)
+        {
+            Debug.Log("lv :"+p.level.ToString());
+        }
+
 	}
 	
 	// Update is called once per frame

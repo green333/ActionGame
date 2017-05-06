f1efef3a817c11e1376543c9eab968e9543f256d
juniata <kenta.github@gmail.com>
2017-05-06 21:19:48 +0900

    武器マスタとプレイヤー基本マスタを読み込みクラスを追加

---
+++ ActionGame/Assets/Script/Other/LoadPlayerBaseMaster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadPlayerBaseMaster{
+
+    /// <summary>
+    /// インスタンス
+    /// </summary>
+    static readonly LoadPlayerBaseMaster instance = new LoadPlayerBaseMaster();
+
+    /// <summary>
+    /// プレイヤー基本マスタを格納する変数
+    /// </summary>
+    PlayerBaseMaster master;
+
+    /// <summary>
+    /// インスタンスを取得
+    /// </summary>
+    static public LoadPlayerBaseMaster Instace { get { return instance; } }
+
+    /// <summary>
+    /// 初期化時にプレイヤー基本マスタを読み込む
+    /// </summary>
+    public void Initialize()
+    {
+        if (null == (master = Resources.Load<PlayerBaseMaster>("MasterData/PlayerBaseMaster")))
+        {
+            Debug.Log("failed to Resources.Load<PlayerBaseMaster>");
+        }
+    }
+}
---
+++ ActionGame/Assets/Script/Other/LoadWeaponMaster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadWeaponMaster{
+
+    /// <summary>
+    /// インスタンス
+    /// </summary>
+    static readonly LoadWeaponMaster instance = new LoadWeaponMaster();
+
+    /// <summary>
+    /// 武器マスタを格納する変数
+    /// </summary>
+    WeaponMaster master;
+
+    /// <summary>
+    /// インスタンスを取得
+    /// </summary>
+    static public LoadWeaponMaster Instace { get { return instance; } }
+
+    /// <summary>
+    /// 初期化時に武器マスタを読み込む
+    /// </summary>
+    public void Initialize()
+    {
+        if (null == (master = Resources.Load<WeaponMaster>("MasterData/WeaponMaster")))
+        {
+            Debug.Log("failed to Resources.Load<WeaponMaster>");
+        }
+    }
+
+}

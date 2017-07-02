8ba072c7b050988e36493983b6daa58aa418da1e
juniata <kenta.github@gmail.com>
2017-07-02 13:56:22 +0900

    マスタ読み込みクラスの修正
    一部コメントの修正。
    
    複数の行データを取得する処理を作成

--- ActionGame/Assets/Script/Editor/TextMasterManager/TextMasterManagerTest.cs
+++ ActionGame/Assets/Script/Editor/TextMasterManager/TextMasterManagerTest.cs
@@ -22,7 +22,7 @@ public class TextMasterManagerTest : TextMasterManager
         new TestCaseData("\"name\":\"薬草\",\"effect\":100","{\"name\":\"薬草\",\"id\":4,\"kind\":\"消費アイテム\",\"effect\":100,\"desc\":\"体力を１００回復する\"}").SetName("値がstringのパラメータの検索を行い、一致するデータがあった時のテスト"),
        
         // 一致するデータが無かったときのテスト
-        new TestCaseData("",string.Empty).SetName("値がstringのパラメータの検索を行い、一致するデータがなかった時のテスト"),
+        new TestCaseData("\"empty\"",string.Empty).SetName("値がstringのパラメータの検索を行い、一致するデータがなかった時のテスト"),
     };
 
     /// <summary>
@@ -34,6 +34,64 @@ public class TextMasterManagerTest : TextMasterManager
         base.Open(filename);
         Assert.AreEqual(base.Search(selectStr), funcRetResult);
         base.Close();
+    }
 
+
+
+    /// <summary> SearchList()のテスト結果 </summary>
+    public static TestCaseData[] searchListProvider = new[]
+    {
+        // すべての検索に一致するデータがあった時のテスト
+        new TestCaseData(
+            new string[]
+            {
+                "\"name\":\"薬草\",\"id\":4",
+                "\"name\":\"薬草_大\""
+            },
+            new string[]
+            {
+                "{\"name\":\"薬草_大\",\"id\":3,\"kind\":\"消費アイテム\",\"effect\":100,\"desc\":\"体力を１００回復する\"}",
+                "{\"name\":\"薬草\",\"id\":4,\"kind\":\"消費アイテム\",\"effect\":100,\"desc\":\"体力を１００回復する\"}",
+            }
+            ).SetName("すべての検索に一致するデータがあった時のテスト"),
+
+        // 一部の検索に一致するデータがあった時のテスト
+        new TestCaseData(
+            new string[]
+            {
+                "\"name\":\"薬草\",\"id\":4",
+                "\"name\":\"薬草_ないよ\""
+            },
+            new string[]
+            {
+                "{\"name\":\"薬草\",\"id\":4,\"kind\":\"消費アイテム\",\"effect\":100,\"desc\":\"体力を１００回復する\"}",
+                string.Empty,
+            }
+            ).SetName("一部の検索に一致するデータがあった時のテスト"),
+
+        // 一致するデータが一つも無かった時のテスト
+        new TestCaseData(
+            new string[]
+            {
+                "\"name\":\"薬草ないよ\",\"id\":4",
+                "\"name\":\"薬草_ないよ\""
+            },
+            new string[]
+            {
+                string.Empty,
+                string.Empty,
+            }
+            ).SetName("一致するデータが一つも無かった時のテスト"),
+   };
+
+    /// <summary>
+    /// SearchList()のテストを行う
+    /// </summary>
+    [TestCaseSource("searchListProvider")]
+    public void TestSearchList(string[] selectStrList,string[] funcRetResult)
+    {
+        base.Open(filename);
+        Assert.AreEqual(base.SearchList(selectStrList), funcRetResult);
+        base.Close();
     }
 }
--- ActionGame/Assets/Script/Other/TextMasterManager.cs
+++ ActionGame/Assets/Script/Other/TextMasterManager.cs
@@ -15,12 +15,12 @@ public class TextMasterManager : TexLoader
     // といったコードを書く場合などがあるため。
 
     /// <summary>
-    /// 指定した文字列に一致する行データを返す。
+    /// 指定した文字列に一致する行データを取得する
     /// </summary>
     /// <param name="selectStr">検索を行う文字列(複数ある場合はカンマ区切り)</param>
     /// <returns>
-    /// 一致するデータがあった時:   一致した文字列データ 
-    /// 一致するデータが無かった時: string.Empty
+    /// 一致するデータがあるば行データ
+    /// 一致するデータがなければstring.Empty
     /// </returns>
     protected string Search(string selectStr)
     {
@@ -33,28 +33,13 @@ public class TextMasterManager : TexLoader
 
         // 検索を行う文字列の対象数
         int selectStrCount = selectStrList.Length;
-        
-        // 一致したデータ数と検索を行う文字列の対象数の比較チェックを行うための変数
-        int matchCount = 0;
 
         while (getLine != string.Empty)
         {
-            // カンマ区切りで検索を行い、見つからなかった場合isFoundフラグをにfalseを設定する。
-            // カンマ区切りでの検索を行った後、isFoundがtrueのままなら
-            foreach (string str in selectStrList)
+            if(string.Empty != (ret = commaStrSearch(getLine, selectStrList, selectStrCount)))
             {
-                if (getLine.IndexOf(str) != -1)
-                {
-                    ++matchCount;
-                }
-            }
-
-            if (selectStrCount == matchCount)
-            {
-                ret = getLine;
                 break;
             }
-            matchCount = 0;
             getLine = base.GetLine();
         }
 
@@ -62,42 +47,107 @@ public class TextMasterManager : TexLoader
     }
 
     /// <summary>
-    /// 複数のデータを取得
-    /// 指定した行数で配列生成するが指定した行数分読み込めなかった場合、配列の中にはstring.Emptyが格納されている。
+    /// 指定した文字列リストに一致する行データを複数取得する
     /// </summary>
-    /// <param name="valList"></param>
-    /// <returns></returns>
-    protected string[] SearchList(string[] valList)
+    /// <param name="selectStrList">検索を行う文字列リスト(複数ある場合は要素をカンマ区切りで格納する)</param>
+    /// <param name="selectStrCount">検索対象の行データ取得数</param>
+    /// <returns>
+    /// 一致するデータがある時は行データ
+    /// 一致するデータはstring.Emptyが格納される
+    /// </returns>
+    protected string[] SearchList(string[] selectStrList,int selectStrCount = 10)
     {
-        // 長さ valList.Lengthの配列をstring.Emptyで初期化
-        string[] ret =  Enumerable.Repeat<string>(string.Empty, valList.Length).ToArray();
+        string[] ret = Enumerable.Repeat<string>(string.Empty, selectStrList.Length).ToArray();
 
-        string getLine = base.GetLine();
+        // 検索対象の文字列を配列で取得する
+        string[] getLineList = base.GetLine(selectStrCount);
 
-        int cheked = 0;
-        while (getLine != null)
+        // 検索する文字列をListにする
+        List<string> selectStr = new List<string>(selectStrList);
+        
+        // カンマ区切りした文字列配列を格納する
+        string[] splistStr = null;
+
+        bool isEnd = false;
+        int index = 0;
+        while(true)
         {
-            foreach(string str in valList)
+            foreach(string getLine in getLineList)
             {
-                if (getLine.IndexOf(str) != -1)
+                // 行データが途中で取得できてなかったときは、ファイル読み込みが終端まで行った時だけなので
+                // isEndフラグをたてる
+                if (getLine == string.Empty)
                 {
-                    ret[cheked++] = getLine;
+                    isEnd = true;
+                    break;
+                }
+
+                for(int i = 0; i < selectStr.Count; ++i)
+                {
+                    splistStr = selectStr[i].Split(',');
+
+                    // カンマ区切りした文字列配列に一致する行データを取得する
+                    if(string.Empty != (ret[index] = commaStrSearch(getLine, splistStr, splistStr.Length)))
+                    {
+                        // 一致する行データを取得できたので、検索対象から外してbreakする
+                        selectStr.Remove(selectStr[i]);
+                        ++index;
+                        break;
+                    }
+                }
+
+                if(index == selectStrList.Length)
+                {
+                    isEnd = true;
                     break;
                 }
             }
 
-            // 検索したい数と同じ数だけ取得できたらbreakする
-            if(cheked == valList.Length)
+            if (isEnd)
             {
                 break;
             }
 
-            getLine = base.GetLine();
+            // 次の検索対象を取得する
+            getLineList = base.GetLine(selectStrCount);
         }
 
         return ret;
     }
 
+
+    /// <summary>
+    /// カンマ区切りされた文字列リストに一致する行データを取得する
+    /// </summary>
+    /// <param name="getLine">検索対象の行データ</param>
+    /// <param name="commaStr">カンマ区切りされた文字列リスト</param>
+    /// <param name="commaStrLength">カンマ区切りされた文字列リストの長さ</param>
+    /// <returns>
+    /// 一致するデータがあれば行データ
+    /// 一致するデータがなければstring.Empty
+    /// </returns>
+    private string commaStrSearch(string getLine,string[] commaStr,int commaStrLength)
+    {
+        string ret = string.Empty;
+
+        // 一致したデータ数と検索を行う文字列の対象数の比較チェックを行うための変数
+        int matchCount = 0;
+
+        foreach (string str in commaStr)
+        {
+            if (getLine.IndexOf(str) != -1)
+            {
+                ++matchCount;
+            }
+        }
+
+        if (commaStrLength == matchCount)
+        {
+            ret = getLine;
+        }
+
+        return ret;
+    }
 
     /// <summary>
     /// 変数名と値をjson文字列に変換する
